41cb754bd57de8eed17be56c23a8eda5afc4583a
G-Dark <97871483+ntproject432@users.noreply.github.com>
2022-01-23 14:03:42 +0700

    Add files via upload

---
+++ [WINDOW] START.bat
@@ -0,0 +1,7 @@
+# Author : Evil Nguyễn
+# Copyright (c) Evil Nguyễn
+# Script chạy bot
+echo "Dang chay kb2abot bootloader . . ."
+chcp 65001 > NUL
+npm start
+read -p "Nhan phim ENTER de thoat . . ."
